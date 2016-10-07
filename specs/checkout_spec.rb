97e565bd2a069f6ec4c277346f249bcd85eb0a83
Paul Dalton <paul@p3d.co.uk>
2016-10-07 18:06:42 +0100

    add specs for Checkout.total

--- specs/checkout_spec.rb
+++ specs/checkout_spec.rb
@@ -32,9 +32,33 @@ describe Checkout do
       it 'appends the new item to the contents' do
         item2 = Item.new(code: '002', name: 'Test2', price: 2)
         co.scan(item2)
-        co.contents.must_equal [item1, item2] 
+        co.contents.must_equal [item1, item2]
       end
     end
 
   end
+
+  describe "#total" do
+    let(:co) { Checkout.new([]) }
+    let(:item1) { Item.new(code: '001', name: 'Test', price: 1) }
+    let(:item2) { Item.new(code: '002', name: 'Test2', price: 2) }
+
+    describe "when there are no items in the basket" do
+
+      it "returns 0" do
+        co.total.must_equal 0
+      end
+
+    end
+
+    describe "when there are items in the basket" do
+
+      it "returns the sum of the prices" do
+        co.scan(item1)
+        co.scan(item2)
+        co.total.must_equal item1.price+item2.price
+      end
+    end
+  end
+
 end
